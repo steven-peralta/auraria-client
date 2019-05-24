50c06c56581b5ffacc003543b8e257caa5eb513e
k9rosie <k9rosie@gmail.com>
2019-05-24 18:25:23 -0500

    library configuration

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        library: 'auraria-client',
+        libraryTarget: 'umd'
     },
     module: {
         rules: [
