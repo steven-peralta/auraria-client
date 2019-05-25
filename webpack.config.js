bc934a7df82b44ea81be94d962dd9fa017214efa
k9rosie <k9rosie@gmail.com>
2019-05-24 20:09:00 -0500

    w4e

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         library: 'auraria',
         libraryTarget: 'umd',
-        libraryExport: 'default'
     },
     module: {
         rules: [
